fb175fe33b4e567fc7059396483f0047e89c475c
Dan <pinerodanchristian@gmail.com>
2022-10-24 15:00:20 +0800

    letter D

--- print_name.py
+++ print_name.py
@@ -5,3 +5,10 @@ str1 = "DAN"
 print_D = [[" " for i in range(5)] for j in range(5)]
 print_A = [[" " for i in range(5)] for j in range(5)]
 print_N = [[" " for i in range(5)] for j in range(5)]
+
+# code for letter D
+for row in range(5):
+    for col in range(5):
+        if (col==0) or (col==4 and (row!=0 and row!=4)) or (row==0 or row==4) and (col>0 and col<4):
+            print_D[row][col]= "*"
+
