0e584762ae5aa84387fa1e0bf02466c599b7b3e9
Dan <pinerodanchristian@gmail.com>
2022-10-24 14:59:21 +0800

    name dan

--- print_name.py
+++ print_name.py
@@ -1,3 +1,7 @@
 # Assignment 1:
 # Create a program that will print your nickname using only asterisk character (*)
 
+str1 = "DAN"
+print_D = [[" " for i in range(5)] for j in range(5)]
+print_A = [[" " for i in range(5)] for j in range(5)]
+print_N = [[" " for i in range(5)] for j in range(5)]
